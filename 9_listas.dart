e979c490c27057f405c70f4bb63aba532757c513
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-11 16:19:32 -0500

    Listas

---
+++ 9_listas.dart
@@ -0,0 +1,26 @@
+main() {
+  List personajes = ['batman', 'superman'];
+  print(personajes);
+
+  print("------------");
+  List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
+  print(num);
+
+  print("------------");
+  personajes.add("Edison");
+  print(personajes);
+
+  print("------------");
+  num.add(15);
+  print(num);
+
+  print("------------");
+  print(num[3]);
+  print(personajes[1]);
+
+  print("------------");
+  num.remove(5);
+  print(num);
+  personajes.remove("batman");
+  print(personajes);
+}
