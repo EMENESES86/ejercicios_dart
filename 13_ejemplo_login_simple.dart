c616d19bc0e7aa404733c8296ef6670cfa678fca
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-11 16:22:05 -0500

    Ejemplo login simple

---
+++ 13_ejemplo_login_simple.dart
@@ -0,0 +1,14 @@
+main() {
+  var user = "Edison";
+  var pass = "123asd";
+
+  if (user == "Edison" && pass == "123asd") {
+    print("Hola Edison");
+  } else {
+    if (user != "Edison") {
+      print("Usuario incorrecto");
+    } else {
+      print("Pass incorrecto");
+    }
+  }
+}
