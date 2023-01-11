4b007646bfbed5424331f00da390f33b57e61540
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-11 16:12:53 -0500

    Switch

---
+++ 6_switch.dart
@@ -0,0 +1,45 @@
+// main() {
+//   String pais = "Canada";
+
+//   switch (pais) {
+//     case "Canada":
+//       print("1er case Correcto");
+//       break;
+//     case "Colombia":
+//       print("2do case Incorrecto");
+//       break;
+//     case "Ecuador":
+//       print("3er case Incorrecto");
+//       break;
+
+//     default:
+//       print("Default Valor no encontrado");
+//       break;
+//   }
+
+// }
+
+// void main() {
+//     int op = 1;
+//   switch (op) {
+//     case 1:
+//       print("Suma");
+//       break;
+
+//     case 2:
+//       print("Resta");
+//       break;
+
+//     case 3:
+//       print("Mult");
+//       break;
+
+//     case 4:
+//       print("Div");
+//       break;
+
+//     default:
+//       print("Operacion no encontrada");
+//       break;
+//   }
+// }
