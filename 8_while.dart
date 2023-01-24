e84e97e47b46baa6d85f095897b809e1eb021de7
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-24 17:45:18 -0500

    Actualizado

--- 8_while.dart
+++ 8_while.dart
@@ -49,36 +49,45 @@
 // }
 
 // -----------------Ejercicio-------------------
-import 'dart:io';
+// import 'dart:io';
 
-main() {
-  while (true) {
-    stdout.write("Quiere continuar si/no: ");
-    String? respuesta = stdin.readLineSync();
-    if (respuesta == "si") {
-      print("***************Ingreso al programa******************");
-      stdout.write("cuantas tablas: ");
-      int tablas = int.parse(stdin.readLineSync().toString());
-      stdout.write("cuantos valores: ");
-      int maximo = int.parse(stdin.readLineSync().toString());
-      int x = 1;
-      while (x <= tablas) {
-        print("La tabla del $x");
-        int y = 0;
-        while (y <= maximo) {
-          if (y > 0) {
-            var mult = x / y;
-            print("$x / $y = $mult");
-          } else {
-            print("$x / $y = no existe div para cero");
-          }
-          y++;
-        }
-        x++;
-      }
-    } else {
-      print("Salio del programa");
-      break;
-    }
-  }
-}
+// main() {
+//   while (true) {
+//     stdout.write("Quiere continuar si/no: ");
+//     String? respuesta = stdin.readLineSync();
+//     if (respuesta == "si") {
+//       print("***************Ingreso al programa******************");
+//       stdout.write("cuantas tablas: ");
+//       int tablas = int.parse(stdin.readLineSync().toString());
+//       stdout.write("cuantos valores: ");
+//       int maximo = int.parse(stdin.readLineSync().toString());
+//       int x = 1;
+//       while (x <= tablas) {
+//         print("La tabla del $x");
+//         int y = 0;
+//         while (y <= maximo) {
+//           if (y > 0) {
+//             var mult = x / y;
+//             print("$x / $y = $mult");
+//           } else {
+//             print("$x / $y = no existe div para cero");
+//           }
+//           y++;
+//         }
+//         x++;
+//       }
+//     } else {
+//       print("Salio del programa");
+//       break;
+//     }
+//   }
+// }
+
+
+// Ejercicio en clases con WHILE
+// 1.	Realizar un menú que muestre las cuatro operaciones básicas suma, resta, multiplicación y división
+// 2.	Seleccionar por teclado que operación se va a realizar
+// 3.	Ingresar por teclado la cantidad de tablas
+// 4.	Ingresar por teclado la cantidad de operaciones que va a haber en cada tabla
+// 5.	Validar la división
+// 6.	Me vuelva a preguntar si se quiere realizar otra operación
