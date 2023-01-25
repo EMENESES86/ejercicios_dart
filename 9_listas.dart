dbcff821c00e2b24207731a10603b1a8ab9b7471
EDISON MENESES <emeneses.developers@gmail.com>
2023-01-24 20:04:16 -0500

    Actualizado listas

--- 9_listas.dart
+++ 9_listas.dart
@@ -1,28 +1,56 @@
-main() {
-  List personajes = ['batman', 'superman'];
-  print(personajes.length);
-  print(personajes);
+// main() {
+//   List personajes = ['batman', 'superman'];
+//   print(personajes.length);
+//   print(personajes);
+// }
 
-  print("------------");
-  List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
-  print(num);
+// main(){
+//   print("------------");
+//   List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
+//   print(num);  
+// }
 
-  print("------------");
-  personajes.add("Edison");
-  print(personajes);
+// main(){
+//   print("------------");
+//   List personajes = ['batman', 'superman'];
+//   personajes.add("Edison");
+//   print(personajes);
+// }
 
-  print("------------");
-  num.add(15);
-  print(num);
+// main(){
+//   print("------------");
+//   List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
+//   num.add(15);
+//   print(num);
+// }
 
-  print("------------");
-  print(num[3]);
-  print(personajes[1]);
+// main(){
+//     print("------------");
+//   List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
+//   List personajes = ['batman', 'superman'];
+//   print(num[3]);
+//   print(personajes[1]);
+// }
 
-  print("------------");
-  num.remove(5);
-  print(num);
-  personajes.remove("batman");
-  print(personajes);
+// main(){
+//   print("------------");
+//   List<int> num = [1, 5, 2, 3, 4, 5, 6, 5];
+//   num.remove(5);
+//   print(num);
+// }
 
-}
+// main(){
+//   print("------------");
+//   List personajes = ['batman', 'superman'];
+//   personajes.remove("batman");
+//   print(personajes);
+// }
+
+// main(){
+//   // eliminar datos repetidos
+//   print("------------");
+//   List<int> listaOriginal = [1, 2, 3, 3, 4, 5, 5];
+//   List<int> listaSinDuplicados = listaOriginal.toSet().toList();
+//   print(listaOriginal);
+//   print(listaSinDuplicados);
+// }
